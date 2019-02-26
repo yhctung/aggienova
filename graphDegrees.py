6c76460595cd225e0c150a243914cceb3df9d0f9
Yung-Hsin Tung <yhc.tung@gmail.com>
2019-02-26 00:39:59 -0600

    colorcode by type

--- graphDegrees.py
+++ graphDegrees.py
@@ -7,16 +7,16 @@ import pandas                             # for dataframes
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
-# read data into dataframe, SNname is separate array
+# read data into dataframe
 df = pandas.read_csv('SwiftSNweblist.csv', usecols= ['type', 'ra', 'dec'])
-SNname = np.genfromtxt('SwiftSNweblist.csv', delimiter=',',dtype = str, usecols=(0), unpack=True)
 
-#replace empty and random ra/dec with Nan
+#replace empty and random with Nan
 df = df.replace(r'^\s*$', np.nan, regex=True) 
 df = df.replace(r'ra', np.nan, regex=True) 
+df = df.replace(r'Unk', np.nan, regex=True) 
 
-#drop rows with Nan in ra or dec columns
-df = df.dropna(subset=['ra', 'dec']) 
+#drop rows with Nan
+df = df.dropna(subset=['type','ra', 'dec']) 
 
 # reset the index from 0
 df = df.reset_index(drop=True)
@@ -32,8 +32,36 @@ type = df.type
 ra = df.ra
 dec = df.dec
 
-# to graph
-CoordSets = []
+# for color coding
+Ia = []   # red
+Ibc = []  # green
+II = []   # blue
+other = []
+SNcolor = []
+
+for i in range(1,len(type)):
+  current = type[i]
+  if "Ia" in current:
+    Ia.append(i)
+    SNcolor.append('red')
+  elif "Ib" in current:
+    Ibc.append(i)
+    SNcolor.append('green')
+  elif "Ic" in current:
+    Ibc.append(i)
+    SNcolor.append('green')  
+  elif "II" in current:
+    II.append(i)
+    SNcolor.append('blue')   
+  else:
+    other.append(i)
+    #SNcolor.append('temp')
+    ra.drop(index = i)
+    dec.drop(index = i)
+
+SNcolor = np.asarray(SNcolor)
+
+# converting to radians
 radianRA = []
 radianDEC = []
 
@@ -42,15 +70,16 @@ for i in range(1,len(ra)):
   c.ra.wrap_angle = 180 * u.deg
   radianRA.append(m.radians(c.ra.degree))
   radianDEC.append(m.radians(c.dec.degree))
-  CoordSets.append(c)
 
+# convert list to array
 radianRA = np.asarray(radianRA)
 radianDEC = np.asarray(radianDEC)
 
 # graphing
 fig = plt.figure(figsize=(8,6))
 ax = fig.add_subplot(111, projection="mollweide")
-ax.scatter(radianRA, radianDEC)
+ax.scatter(radianRA, radianDEC, color = SNcolor)
 ax.set_xticklabels(['14h','16h','18h','20h','22h','0h','2h','4h','6h','8h','10h'])
 ax.grid(True)
 fig.savefig("map.pdf")
+
